deb41abfccc493e2d5ec1985cc74576e178ca46c
ITOH <to@itoh.at>
2021-07-10 21:51:21 +0200

    that's what makes brains hurting

--- src/tokens/token.ts
+++ src/tokens/token.ts
@@ -7,15 +7,19 @@ export interface State {
   parameters: any[];
 }
 
-export const createQueryState = (tokens: Token[], currentParameterIndex: number = 0): State => {
-  const initialState = {
+export const createQueryState = (
+  tokens: Token[],
+  currentParameterIndex: number = 0,
+  initialState = {
     text: [],
     parameters: [],
-  };
+  }
+): State => {
+  return tokens.reduce((tokenState, token) => {
+    if (Array.isArray(token)) {
+      return createQueryState(token, tokenState.parameters.length + currentParameterIndex, tokenState);
+    }
 
-  return tokens.reduce(
-    (tokenState, token) =>
-      token.reduce(tokenState, tokenState.parameters.length + currentParameterIndex),
-    initialState
-  );
+    return token.reduce(tokenState, tokenState.parameters.length + currentParameterIndex);
+  }, initialState);
 };
